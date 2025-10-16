d348a45202232851535cf1053db8e517075a552e
mufid1012 <mufidghibran1012@gmail.com>
2025-10-16 15:05:14 +0700

    Menambahkan layout utama TataLetakBoxColumnRow ke MainActivity

---
+++ app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.questbasiccomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
+import com.example.questbasiccomposable.ui.TataLetakBoxColumnRow
+import androidx.compose.material3.Text
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposableTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // 🔹 Panggil layout utama
+                    TataLetakBoxColumnRow(
+                        modifier = Modifier.padding(paddingValues = innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposableTheme {
+        Greeting("Android")
+    }
+}
