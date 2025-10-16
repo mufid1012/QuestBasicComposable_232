b76bc6ffcb721f9805fb66514e9f535a46d6cbed
mufid1012 <mufidghibran1012@gmail.com>
2025-10-16 14:53:23 +0700

    Menambahkan teks pada Row pertama dengan tiga komponen.

--- app/src/main/java/com/example/mylayout/TataLetak.kt
+++ app/src/main/java/com/example/mylayout/TataLetak.kt
@@ -70,6 +70,10 @@ fun TataLetakColumnRow(modifier: Modifier) {
             modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+
         }
         Row(
             modifier = modifier.fillMaxWidth(),
