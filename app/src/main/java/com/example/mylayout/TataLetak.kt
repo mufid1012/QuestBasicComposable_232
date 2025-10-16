cf99f49399d3a7e4fa8f6dbd7f27a868394a3103
mufid1012 <mufidghibran1012@gmail.com>
2025-10-16 14:59:47 +0700

    Menambahkan teks pada Row pertama

--- app/src/main/java/com/example/mylayout/TataLetak.kt
+++ app/src/main/java/com/example/mylayout/TataLetak.kt
@@ -116,6 +116,10 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
                 modifier = modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
+                Text(text = "Col1_Row1_Komponen1")
+                Text(text = "Col1_Row1_Komponen2")
+                Text(text = "Col1_Row1_Komponen3")
+
             }
             Row(
                 modifier = modifier.fillMaxWidth(),
