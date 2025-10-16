1fa10cca4531a50d7c395b79cc8716a4813cbb82
mufid1012 <mufidghibran1012@gmail.com>
2025-10-16 15:02:19 +0700

    Menambahkan Box berisi gambar dan teks “My Music”.

--- app/src/main/java/com/example/mylayout/TataLetak.kt
+++ app/src/main/java/com/example/mylayout/TataLetak.kt
@@ -133,7 +133,27 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
 
         Spacer(modifier = Modifier.height(10.dp))
 
-
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
     }
 }
 
